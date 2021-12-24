057b8047494d8811cda3bc722410890385b2edb7
Rajan kumar <krcpr007@gmail.com>
2021-12-24 20:31:03 +0530

    Navbar modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React  from "react";
+import { Link, useNavigate} from "react-router-dom";
 const Navbar= ()=> {
- 
+    const navigate = useNavigate();
+    const auth = localStorage.getItem('user');
+    const handleLogOut =()=>
+    {
+     localStorage.removeItem('user')
+     navigate('/login')
+    }
      return (
-         <div>
-             <ul className="nav-ul">
-                 <li><Link to="/" >Product</Link></li>
-                 <li><Link to="/" >Add Product</Link></li>
+         <nav>
+             <ul className="nav-ul shadow">
+                 <li><Link to="/product" >Product</Link></li>
+                 <li><Link to="/addProduct" >Add Product</Link></li>
+                 <li><Link to="/updateProduct" >Update Product</Link></li>
+                 <li><Link to="/profile" >Profile</Link></li>
                  <li><Link to="/" >Home</Link></li>
-                 <li><Link to="/" >Home</Link></li>
-                 <li><Link to="/" >Home</Link></li>
-                 <li><Link to="/" >Home</Link></li>
+                 {auth?null:<li><Link to="/login" >login</Link></li>}
+                 <li>{auth?(<Link onClick={handleLogOut} to='/login' >Logout</Link>):(<Link to="/signup" >signup</Link>)}</li>
              </ul>
-         </div>
+         </nav>
      )
 }
 export default Navbar; 
