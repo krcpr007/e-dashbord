b99030dc152581148af49a1e1ee76e4210f2a47d
Rajan kumar <krcpr007@gmail.com>
2021-12-24 20:32:35 +0530

    tailwindcss and daisyUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter} from 'react-router-dom'; 
+import {BrowserRouter , Routes, Route} from 'react-router-dom'; 
+import Footer from './components/Footer';
+import Signup from './components/Signup';
+import Home from './components/Home';
+import Login from './components/Login';
+import PrivateComponent from './components/PrivateComponent';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar/>
+      <Routes>
+
+      <Route   element={<PrivateComponent/>}>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/addProduct" element={<h1>This is add product </h1>}/>
+      <Route path="/updateProduct" element={<h1>This is updateProduct page</h1>}/>
+      <Route path="/profile" element={<h1>This is profile page</h1>}/>
+      <Route path="/product" element={<h1>This is product page</h1>}/>
+      </Route>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+      {/* <Footer/> */}
     </div>
     </BrowserRouter>
   );
