a091916242b9f7b132d5a33ebed059f8ca889843
Rajan kumar <krcpr007@gmail.com>
2022-01-04 00:30:45 +0530

    signup and login almost done

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,34 @@
-const express = require("express"); 
-const app = express(); 
+const express = require("express");
+const app = express();
+require("./db/config");
+const User = require("./models/User");
+const cors = require("cors");
 
-app.get("/", (req,resp)=>
-{
-    resp.send("App is Working"); 
-})
-app.listen("5000")
+console.log("App listen at port 5000");
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, resp) => {
+  resp.send("App is Working");
+});
+app.post("/signup", async (req, resp) => {
+  const user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  resp.send(req.body);
+  console.log(result);
+});
+
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No user found" });
+    }
+  }else {
+    resp.send({ result: "No user found" });
+  }
+});
+app.listen(5000);
