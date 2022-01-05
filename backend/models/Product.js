79863654b49647c4ca110d119d6771b9b84ed6a7
Rajan kumar <krcpr007@gmail.com>
2022-01-05 23:04:53 +0530

    Models added

---
+++ backend/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  catogory: {
+    type: String,
+    required: true,
+  },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Product = mongoose.model("products", ProductSchema);
+Product.createIndexes();
+module.exports = Product;
