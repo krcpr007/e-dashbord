172e731c4655bf25b259012a121dc77b2df5f8a7
Rajan kumar <krcpr007@gmail.com>
2022-01-19 13:34:16 +0530

    React toastify added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter , Routes, Route} from 'react-router-dom'; 
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Products from './components/Products';
 import Signup from './components/Signup';
 import Home from './components/Home';
@@ -11,6 +11,10 @@ import Profile from './components/Profile';
 import PrivateComponent from './components/PrivateComponent';
 import AddProduct from './components/AddProduct';
 import YoursProduct from './components/YoursProduct';
+import WishList from './components/WishList';
+import ShopingCart from './components/ShopingCart';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const auth = localStorage.getItem("user");
   const profileData= JSON.parse(auth)
@@ -19,20 +23,27 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
-      <Route   element={<PrivateComponent/>}>
+      <Route element={<PrivateComponent/>}>
       <Route path="/" element={<Home/>}/>
       <Route path="/addProduct" element={<AddProduct/>}/>
       <Route path="/your-product" element={<YoursProduct/>}/>
       <Route path="/updateProduct" element={<h1>This is updateProduct page</h1>}/>
       <Route path="/profile" element={<Profile/>}/>
-      <Route path="/product" element={<Products/>}/>
-      <Route path={`/${profileData.name}/cart`} element={<h1>{profileData.name}</h1>}/>
-      <Route path={`/${profileData.name}/wishList`} element={<h1>{profileData.name} wishlist</h1>}/>
+      <Route path="/products" element={<Products/>}/>
+      {
+        auth? (
+          <>
+          <Route path={`/${profileData.name}/cart`} element={<ShopingCart/>}/>
+          <Route path={`/${profileData.name}/wishList`} element={<WishList/>}/>
+          </>
+        ):null
+      }
       </Route>
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
       </Routes>
       {/* <Footer/> */}
+      <ToastContainer />
     </div>
     </BrowserRouter>
   );
