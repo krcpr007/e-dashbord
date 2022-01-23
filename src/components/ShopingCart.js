4579f9a05628c61ce66a5f5b468bb89a2632031e
Rajan kumar <krcpr007@gmail.com>
2022-01-23 19:13:58 +0530

    bg-change looks good

--- src/components/ShopingCart.js
+++ src/components/ShopingCart.js
@@ -10,15 +10,15 @@ function ShopingCart() {
     <>
      <div className="h-screen ">
     <div className="py-12">
-        <div className="max-w-md mx-auto bg-gray-300 shadow-lg rounded-lg md:max-w-5xl">
+        <div className="max-w-md mx-auto bg-gray-600 shadow-lg rounded-lg md:max-w-5xl">
             <div className="md:flex ">
                 <div className="w-full p-4 px-5 py-5">
                     <div className="md:grid md:grid-cols-3 gap-2 ">
                         <div className="col-span-2 p-5">
                             <h1 className="text-xl font-medium ">Shopping Cart</h1>
-                            <CartItem />
-                            <CartItem />
-                            <CartItem />
+                            <CartItem  price="10k"/>
+                            <CartItem price="10k"/>
+                            <CartItem price="10k"/>
                             <div className="flex justify-between items-center mt-6 pt-6 border-t">
                                 <div className="flex items-center"> <i className="fa fa-arrow-left text-sm pr-2"></i> <Link to="/products" className="text-md font-medium text-blue-500">Continue Shopping</Link></div>
                                 <div className="flex justify-center items-end"> <span className="text-sm font-medium text-gray-400 mr-1">Subtotal:</span> <span className="text-lg font-bold text-gray-800 "> $24.90</span> </div>
@@ -46,18 +46,18 @@ function ShopingCart() {
                             </div>
                             <div className="flex justify-center flex-col pt-3"> 
                                 <label className="text-xs text-gray-400 ">Card Number</label> 
-                                <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4" placeholder="**** **** **** ****" onChange={e=>setCardNum(e.target.value)} maxlength="16" />
+                                <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4" placeholder="**** **** **** ****" onChange={e=>setCardNum(e.target.value)} maxLength="16" />
                             </div>
                             <div className="grid grid-cols-3 gap-2 pt-2 mb-3">
                                 <div className="col-span-2 "> <label className="text-xs text-gray-400">Expiration Date</label>
                                     <div className="grid grid-cols-2 gap-2">
-                                          <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4 px-8" placeholder="mm" onChange={e=>setCardMonth(e.target.value)} maxlength="2"/>
-                                          <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4 px-8" placeholder="yy"onChange={e=>setCardYear(e.target.value)} maxlength="2" />
+                                          <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4 px-8" placeholder="mm" onChange={e=>setCardMonth(e.target.value)} maxLength="2"/>
+                                          <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4 px-8" placeholder="yy"onChange={e=>setCardYear(e.target.value)} maxLength="2" />
                                     </div>
                                 </div>
                                 <div className=""> 
                                     <label className="text-xs text-gray-400">CVV</label> 
-                                    <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4 px-8" placeholder="XXX" maxlength="3"  />
+                                    <input type="text" className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4 px-8" placeholder="XXX" maxLength="3"  />
                                 </div>
                             </div> 
                             <button className="h-12 w-full bg-blue-500 rounded focus:outline-none text-white hover:bg-blue-600">Check Out</button>
