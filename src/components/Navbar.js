07440621439274dd8dc240406144680036c86298
Rajan kumar <krcpr007@gmail.com>
2022-01-23 17:26:04 +0530

    new navbar set style remain

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,64 +1,109 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate,useLocation } from "react-router-dom";
 import {FaCartPlus} from 'react-icons/fa'
 import {BiBookmarkHeart} from 'react-icons/bi'
 const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation(); 
   const auth = localStorage.getItem("user");
-  const profileData= JSON.parse(auth)
+  const profileData= JSON.parse(auth);
   const handleLogOut = () => {
     localStorage.removeItem("user");
     navigate("/login");
   };
+  const pathMatchRoute= (route)=>{
+    if(route===location.pathname){
+      return true;
+    }
+  }
+  // const btn = document.querySelector("button.mobile-menu-button");
+  const menu = document.querySelector(".mobile-menu");
+  const handleMobileView =()=>{
+    menu.classList.toggle("hidden")
+  }
+
   return (
-    <nav>
-      <img src="https://res.cloudinary.com/teepublic/image/private/s--DqPWTsIX--/c_crop,x_10,y_10/c_fit,w_830/c_crop,g_north_west,h_1038,w_1038,x_-104,y_-243/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-215,y_-354/b_rgb:ffffff/c_limit,f_jpg,h_630,q_90,w_630/v1547549360/production/designs/3987359_0.jpg" alt="logo" 
-      className="w-10 float-left mx-3 mt-0 rounded-full"/>
-      {auth ? (
-        <ul className="nav-ul shadow-lg flex">
-          <li>
-            <Link to="/product">Product</Link>
-          </li>
-          <li>
-            <Link to="/your-product">Your Products</Link>
-          </li>
-          <li>
-            <Link to="/addProduct">Add Product</Link>
-          </li>
-          <li>
-            <Link to="/updateProduct">Update Product</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link onClick={handleLogOut} to="/login">
-              Logout ({JSON.parse(auth).name})
-            </Link>
-          </li>
-          <ul className="nav-ul  text-right">
-          <li>
-            <Link to={`/${profileData.name}/wishList`}><BiBookmarkHeart/></Link>
-          </li>
-          <li>
-            <Link to={`/${profileData.name}/cart`}> <FaCartPlus/> </Link>
-          </li>
-          </ul>
-        </ul>
-      ) : (
-        <ul className="nav-ul shadow text-right">
+    <nav className="bg-slate-900 shadow-xl">
+			<div className="max-w-6xl mx-auto px-4">
+				<div className="flex justify-between">
+					<div className="flex space-x-7">
+						<div>
+							{/* <!-- Website Logo --> */}
+							<Link to="/" className="flex items-center py-4 px-2">
+								<img src="https://res.cloudinary.com/teepublic/image/private/s--DqPWTsIX--/c_crop,x_10,y_10/c_fit,w_830/c_crop,g_north_west,h_1038,w_1038,x_-104,y_-243/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-215,y_-354/b_rgb:ffffff/c_limit,f_jpg,h_630,q_90,w_630/v1547549360/production/designs/3987359_0.jpg" alt="Logo" className="h-8 float-left mx-3 mt-0 rounded-full"/>
+								<span className="font-semibold text-gray-500 text-lg">Egle-Dashbord</span>
+							</Link>
+						</div>
+						{/* <!-- Primary Navbar items --> */}
+						<div className="hidden md:flex items-center space-x-1">
+							<Link to="/" className={`py-4 px-2 text-green-500  border-green-500 font-semibold ${pathMatchRoute('/')?'border-b-4':null}`}>Home</Link>
+							<Link to="/products" className={`block text-sm px-2 py-4  transition duration-300 ${pathMatchRoute('/products')?'border-b-4':null} `}>Products</Link>
+							<Link to="/addProduct" className={`block text-sm px-2 py-4  transition duration-300 ${pathMatchRoute('/addProduct')?'border-b-4':null} `}>Add Product</Link>
+							<Link to="/your-product" className={`block text-sm px-2 py-4  transition duration-300 ${pathMatchRoute('/your-product')?'border-b-4':null} `}>Your Products</Link>
+							<Link to="/updateProduct" className={`block text-sm px-2 py-4  transition duration-300 ${pathMatchRoute('/updateProduct')?'border-b-4':null} `}>Update Products</Link>
+						</div>
+					</div>
+					{/* <!-- Secondary Navbar items --> */}
+					<div className="hidden md:flex items-center space-x-3 ">
+						{auth?(
+              <>
+              <Link to='/wishList'>
+               <BiBookmarkHeart className="text-gray-500 hover:text-gray-50"/>
+              </Link>
+              <Link to="/shopingCart">
+               <FaCartPlus className="text-gray-500 hover:text-gray-50 "/>
+              </Link>
+              <Link to="/login" onClick={handleLogOut} className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log Out</Link>
+              </>
+            ):(
+              <>
+              <Link to="/login" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</Link>
+						  <Link to="/signup" className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</Link>
+              </>
+            )}
+					</div>
+					{/* <!-- Mobile menu button --> */}
+					<div className="md:hidden flex items-center">
+						<button className="outline-none mobile-menu-button" onClick={handleMobileView}>
+						<svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
+							x-show="!showMenu"
+							fill="none"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button>
+					</div>
+				</div>
+			</div>
+			{/* <!-- mobile menu --> */}
+			<div className="hidden mobile-menu">
+				<ul className="">
           <li className="">
-            <Link to="/signup">Signup</Link>
+          <Link to="/" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</Link>
           </li>
-          <li className="mx-2">
-            <Link to="/login">Login</Link>
+          <li>
+          <Link to="/products" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Products</Link>
           </li>
-        </ul>
-      )}
-    </nav>
+          <li>
+          <Link to="/addProduct" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Add Product</Link>
+          </li>
+          <li>
+          <Link to="/your-product" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Your Products</Link>
+          </li>
+          <li>
+          <Link to="/updateProduct" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Update Products</Link>
+          </li>
+				</ul>
+			</div>
+			<script>
+
+			</script>
+		</nav>
   );
 };
 export default Navbar;
