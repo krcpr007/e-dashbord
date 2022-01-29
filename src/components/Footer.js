1a9d872454ccbbcc4f4776abff14a5c01ca934df
Rajan kumar <krcpr007@gmail.com>
2022-01-29 23:52:56 +0530

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import {FaTwitter} from "react-icons/fa"
+import {AiFillFacebook} from "react-icons/ai"
+import {BsYoutube} from 'react-icons/bs';
+import {Link} from "react-router-dom";
 function Footer() {
     return (
-        <footer className="footer text-center text-3xl p-5 absolute bottom-0">
-            <h3>E-COMMERCE DASHBORD</h3>
-        </footer>
+      <footer className="p-10 footer bg-base-900 text-base-content footer-center">
+      <div className="grid grid-flow-col gap-4">
+        {/* <a className="link link-hover">About us</a> 
+        <a className="link link-hover">Contact</a> 
+        <a className="link link-hover">Jobs</a> 
+        <a className="link link-hover">Press kit</a> */}
+      </div> 
+      <div>
+        <div className="grid grid-flow-col gap-4">
+          <Link to='/' >
+            <FaTwitter className='text-xl text-gray-400 hover:text-slate-900' />
+          </Link> 
+          <Link to='/' >
+            <AiFillFacebook className='text-xl text-gray-400 hover:text-slate-900'/>
+          </Link> 
+          <Link to='/' >
+            <BsYoutube className='text-xl text-gray-400 hover:text-slate-600 shadow-2xl'/>
+          </Link> 
+        </div>
+      </div> 
+      <div>
+        <p>Copyright © 2021 - All right reserved by ACME Industries Ltd</p>
+      </div>
+    </footer>
+    
     )
 }
 
