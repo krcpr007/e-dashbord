cc98a62481ce8add48696af910cdf460456b2cae
Rajan kumar <krcpr007@gmail.com>
2022-02-15 16:54:20 +0530

    update api created

--- backend/index.js
+++ backend/index.js
@@ -3,37 +3,33 @@
 // const upload = multer({ storage })
 // const { response } = require("express");
 // const { replaceOne } = require("./models/User");
-const express = require('express');
+const express = require("express");
 const app = express();
 require("./db/config");
 const User = require("./models/User");
 const Product = require("./models/Product");
-const Cart = require('./models/ShopingCart'); 
-const Wishlist = require('./models/Wishlist'); 
+const Cart = require("./models/ShopingCart");
+const Wishlist = require("./models/Wishlist");
 const cors = require("cors");
 console.log("App listen at port 5000");
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, resp) => {
-
   resp.send("App is Working");
-
 });
 //Api for register
 app.post("/signup", async (req, resp) => {
-
   try {
     const user = new User(req.body);
     let result = await user.save();
     result = result.toObject();
-    if(result){
+    if (result) {
       delete result.password;
       resp.send(req.body);
       console.log(result);
-    }else{
+    } else {
       console.log("User already register");
     }
-    
   } catch (e) {
     resp.send("Something Went Wrong");
   }
@@ -51,10 +47,8 @@ app.post("/login", async (req, resp) => {
     } else {
       resp.send({ result: "No user found" });
     }
-    
   } catch (error) {
     resp.send({ result: "No user found" });
-    
   }
 });
 // api for add product
@@ -64,104 +58,114 @@ app.post("/add-product", async (req, resp) => {
     let result = await product.save();
     resp.send(result);
   } catch (error) {
-   resp.send("Error")
+    resp.send("Error");
   }
   // console.log(result, req.files);
-
-
 });
 // all products available on database
-app.get('/all-products',async(req,resp)=>{
-   let product = await Product.find({});
-   if(product){
-     resp.send(product)
-   }else{
-     resp.send({result:"Doesn't Have data"})
-   }
-})
-app.get('/search/:query', async(req,resp)=>{
-  let product = await Product.find({name:req.params.query })
-   if(product){
-     resp.send(product); 
-   }
-})
-
-app.post('/yours-products',async(req,resp)=>{
-
-  let product = await Product.find(req.body);
-  if(product){
-    resp.send(product)
-  }else{
-    resp.send({result:"Doesn't Have data"})
+app.get("/all-products", async (req, resp) => {
+  let product = await Product.find({});
+  if (product) {
+    resp.send(product);
+  } else {
+    resp.send({ result: "Doesn't Have data" });
   }
-})
+});
+// Searching api for products
 
-app.delete('/delete-product/:id', async(req,resp)=>{
-       try {
-         let product = await Product.findById({_id:req.params.id}); 
-         if(!product) return resp.send(404).send("Not Found")
-         product= await Product.findByIdAndDelete(req.params.id)
-         resp.json({"msg":"Deleted",product:product})
-       } catch (error) {
-         console.log(error); 
-         resp.send("Something Went Wrong");
-       }
-})
-//  Getting a single product 
-app.get('/products/:id', async(req,resp)=>{
-   try {
-     let product = await Product.findById({_id:req.params.id});
-     resp.send(product);
-   } catch (error) {
-     console.log(error);
-     resp.send({result:"not found"});
-   }
+app.get("/search/:query", async (req, resp) => {
+  let product = await Product.find({
+    $or: [
+      { name: { $regex: req.params.query } },
+      { compnay: { $regex: req.params.query } },
+      { catogory: { $regex: req.params.query } },
+      { price: { $regex: req.params.query } },
+    ],
+  });
+  if (product) {
+    resp.send(product);
+  }
+});
+// your own products
+app.post("/yours-products", async (req, resp) => {
+  let product = await Product.find(req.body);
+  if (product) {
+    resp.send(product);
+  } else {
+    resp.send({ result: "Doesn't Have data" });
+  }
+});
+// delete a product via thier owner
+app.delete("/delete-product/:id", async (req, resp) => {
+  try {
+    let product = await Product.findById({ _id: req.params.id });
+    if (!product) return resp.send(404).send("Not Found");
+    product = await Product.findByIdAndDelete(req.params.id);
+    resp.json({ msg: "Deleted", product: product });
+  } catch (error) {
+    console.log(error);
+    resp.send("Something Went Wrong");
+  }
+});
+//  Getting a single product
+app.get("/products/:id", async (req, resp) => {
+  try {
+    let product = await Product.findById({ _id: req.params.id });
+    resp.send(product);
+  } catch (error) {
+    console.log(error);
+    resp.send({ result: "not found" });
+  }
   // if(product){
   // }else if(!product){
   //   resp.status(404);
   //   resp.send({result:"Not found"});
   // }
+});
 
-})
-
-// app.put("/update-product/:id",async(req,resp)=>{
-//     // let product = await Product.updateOne({
-//     //   _id:req.params.id}, {$set:req.body})
-//     //   resp.send({updated});
-//     // let product = await  Product.findByIdAndUpdate({_id:req.params.id}, {$set:req.body})
-//     // resp.send(req.body);
-// })
-app.post('/wishlists',async(req,resp)=>{
-   let wishlist = new Wishlist(req.body); 
-   let result = await wishlist.save(); 
-   result = result.toObject();
-   if(result){
-     resp.send(result);
-   }
-})
-app.get("/get-wishlist", async (req,resp)=>{
-  let wishlist = await Wishlist.find(req.body); 
-  if(wishlist){
-    resp.send(wishlist); 
-  }else{
-    resp.send({"error":"No Data found"})
+app.put("/update-product/:id", async (req, resp) => {
+  try {
+    let product = await Product.updateOne(
+      {
+        _id: req.params.id,
+      },
+      { $set: req.body }
+    );
+    resp.send({ updated: true, product: product });
+  } catch (error) {
+    console.log(error);
   }
-})
-app.post('/add-to-cart/:id', async(req,resp)=>{
-  let product = new Cart(req.body); 
-  let result = await product.save(); 
-  result = result.toObject(); 
-  if(result){
-    resp.send(result); 
+});
+app.post("/wishlists", async (req, resp) => {
+  let wishlist = new Wishlist(req.body);
+  let result = await wishlist.save();
+  result = result.toObject();
+  if (result) {
+    resp.send(result);
   }
-})
-app.post('/get-cart', async(req,resp)=>{
-    let cartItem  = await Cart.find(req.body); 
-    if(cartItem){
-      resp.send(cartItem); 
-    }else{
-      resp.send({"error":"No Data found"})
-    }
-
-})
+});
+app.get("/get-wishlist", async (req, resp) => {
+  let wishlist = await Wishlist.find(req.body);
+  if (wishlist) {
+    resp.send(wishlist);
+  } else {
+    resp.send({ error: "No Data found" });
+  }
+});
+app.post("/add-to-cart/:id", async (req, resp) => {
+  let product = new Cart(req.body);
+  let result = await product.save();
+  result = result.toObject();
+  if (result) {
+    resp.send(result);
+  }
+});
+app.post("/get-cart", async (req, resp) => {
+  let cartItem = await Cart.find(req.body);
+  if (cartItem) {
+    resp.send(cartItem);
+  } else {
+    resp.send({ error: "No Data found" });
+  }
+});
 app.listen(5000);
