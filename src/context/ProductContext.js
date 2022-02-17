f5d62d7b632902cf3066f0dad39eecd75d297d5f
Rajan kumar <krcpr007@gmail.com>
2022-02-17 14:54:32 +0530

    product searching

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,70 +1,74 @@
-import { createContext, useState ,useEffect } from "react";
-import { toast } from "react-toastify";
-const ProductContext= createContext(); 
-
-export function ProductProvider ({ children }){
+import { createContext, useState } from "react";
+import { toast } from "react-toastify"; 
+const ProductContext = createContext();
+// require('dotenv').config()
+export function ProductProvider({ children }) {
+  const auth = localStorage.getItem("user");
+  const userData = JSON.parse(auth);
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState(""); 
-// Here fetching all products in database 
-  const allProducts =()=>{ 
+  const [search, setSearch] = useState("");
+  // Here fetching all products in database
+  const allProducts = () => {
     fetch(`${process.env.REACT_APP_PORT}/all-products`)
       .then((resp) => resp.json())
       .then((json) => {
         setData(json);
-        console.log(json);
+        // console.log(json);
       })
       .catch((err) => console.log(err));
-  }
-  useEffect(() => {
-    allProducts();
-  }, []);
+  };
   // Searching for product
-  const SearchQuery = async (e)=>{
+  const SearchQuery = async (e) => {
     e.preventDefault();
-   fetch(`${process.env.REACT_APP_PORT}/search/${search}`)
-   .then((resp)=>resp.json())
-   .then((json)=>{
-     setData(json); 
-     console.log(json); 
-    //  toast.success("Item found");
-   }).catch(err=>{
-     console.log(err); 
-     toast.error("Something wrong")
-   })
-  }
-  // clear text in input where searching product 
-  const handleOnClear =(e)=>{
-   e.preventDefault();
-   setSearch(""); 
-   allProducts();
- }
-
-
-
-
-
-
-
-
-
-
-
-    // Delete a product 
-    // const handleDeleteProduct = async ()=>{
-    //     console.log(id);
-    //          await fetch(`${process.env.REACT_APP_PORT}/delete-product/${id}`, {
-    //           method:"Delete", 
-    //           headers:{
-    //             'Content-Type':'application/json'
-    //           }, 
-              
-    //         })
-    //      toast.success("Deleted");
-    //   }
-return (
-<ProductContext.Provider value={{ data,setSearch, handleOnClear ,SearchQuery, search}}>
- {children}
- </ProductContext.Provider>
-)}
-//  ProductProvider; 
-export  default ProductContext ;
+  fetch(`http://localhost:5000/search/${search}`)  //http://localhost:5000/search
+      .then((resp) => resp.json())
+      .then((json) => {
+        setData(json);
+        console.log(json);
+         toast.success("Item found");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Something wrong");
+      });
+  };
+  // clear text in input where searching product
+  const handleOnClear = (e) => {
+    e.preventDefault();
+    setSearch("");
+    allProducts();
+  };
+  //  findig user registerd product 
+  const yourProduct = async()=>{
+    const userId = userData._id;
+    let result = await fetch('http://localhost:5000/yours-products', {
+        method:'POST', 
+        body:JSON.stringify({userId}),
+        headers:{
+            'Content-Type':'application/json'
+        },
+    })
+        result = await result.json(); 
+        if(result){
+          setData(result)
+          console.warn(result);
+        }
+}
+  return (
+    <ProductContext.Provider
+      value={{
+        data,
+        setSearch,
+        handleOnClear,
+        yourProduct,
+        allProducts,
+        SearchQuery,
+        search,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+}
+//  ProductProvider;
+export default ProductContext;
